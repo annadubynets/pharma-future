2ac79bf88428886adf2f4de8efc762ba16b37ad7
Anna Dubynets <arogulina@gmail.com>
2021-08-12 17:12:36 +0300

    fix

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+if ($('.owl-carousel').length > 0) {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        autoplay: false,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            720: {
+                items: 3,
+                nav: false,
+                loop: false
+            }
+        }
+    })
+}
